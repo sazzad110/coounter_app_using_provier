7df0ee5dfc9435e23bd776f8d20c9b55f9c8acf8
sazzad110 <sazzadhasan610@gmail.com>
2024-01-22 16:38:58 +0600

    Background color and Card added

--- lib/home.dart
+++ lib/home.dart
@@ -1,58 +1,99 @@
-// Simple counter using setState
-
-import 'package:coounter_app_using_provier/list_provider.dart';
-import 'package:coounter_app_using_provier/second.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:coounter_app_using_provier/list_provider.dart';
+import 'package:coounter_app_using_provier/second.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Consumer<NumberListProvider>(
-        builder: (context,numbersProvierModel,child) =>  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          numbersProvierModel.add();
-        },
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(title: Text("Counter App")),
-      body: SizedBox(
+      builder: (context, numbersProviderModel, child) => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            numbersProviderModel.add();
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title: Text(
+            "Counter App",
+            style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.indigo,
+          elevation: 0, // No shadow
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                numbersProvierModel.numbers.last.toString(),
-                style: TextStyle(fontSize: 30),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(vertical: 20),
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Text(
+                  numbersProviderModel.numbers.last.toString(),
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
               ),
               Expanded(
-                child: ListView.builder(
-                  itemCount: numbersProvierModel.numbers.length,
-                  itemBuilder: (context, index) {
-                    return Text(
-                      numbersProvierModel.numbers[index].toString(),
-                      style: TextStyle(fontSize: 30),
-                    );
-                  },
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  child: ListView.builder(
+                    itemCount: numbersProviderModel.numbers.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: Card(
+                          color: Colors.lightBlueAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          elevation: 3,
+                          child: ListTile(
+                            title: Text(
+                              numbersProviderModel.numbers[index].toString(),
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const Second()));
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const Second()),
+                  );
                 },
                 child: Text("Next Page"),
               )
             ],
           ),
-      ),
         ),
+      ),
     );
   }
 }
--- lib/second.dart
+++ lib/second.dart
@@ -1,9 +1,9 @@
-import 'package:coounter_app_using_provier/list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:coounter_app_using_provier/list_provider.dart';
 
 class Second extends StatefulWidget {
-  const Second({super.key});
+  const Second({Key? key}) : super(key: key);
 
   @override
   State<Second> createState() => _SecondState();
@@ -13,43 +13,86 @@ class _SecondState extends State<Second> {
   @override
   Widget build(BuildContext context) {
     return Consumer<NumberListProvider>(
-        builder: (context,numbersProvierModel,child) =>  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          numbersProvierModel.add();
-        },
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(title: Text("second page")),
-      body: SizedBox(
-          child: Column(
-            children: [
-              Text(
-                numbersProvierModel.numbers.last.toString(),
-                style: TextStyle(fontSize: 30),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: numbersProvierModel.numbers.length,
-                  itemBuilder: (context, index) {
-                    return Text(
-                      numbersProvierModel.numbers[index].toString(),
-                      style: TextStyle(fontSize: 30),
-                    );
-                  },
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const Second()));
-                },
-                child: Text("Next Page"),
-              )
-            ],
-          ),
-      ),
+      builder: (context, numbersProviderModel, child) => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            numbersProviderModel.add();
+          },
+          child: Icon(Icons.add),
         ),
+        appBar: AppBar(
+          title: Text(
+            "Second Page",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue, // You can customize the color
+          elevation: 0, // No shadow
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: SizedBox(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Text(
+                    numbersProviderModel.numbers.last.toString(),
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    child: ListView.builder(
+                      itemCount: numbersProviderModel.numbers.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 8),
+                          child: Card(
+                            color: Colors.lightBlueAccent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            elevation: 3,
+                            child: ListTile(
+                              title: Text(
+                                numbersProviderModel.numbers[index].toString(),
+                                style: TextStyle(fontSize: 24),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     Navigator.of(context).push(
+                //       MaterialPageRoute(builder: (context) => const Second()),
+                //     );
+                //   },
+                //   child: Text("Next Page"),
+                // )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
